4a203c509e73f383f9ae8708a1bfc83539e942a5
Sachak <Sacha.konskier@gmail.com>
2024-03-24 15:57:04 +0100

    init tailwind and shadecn

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <NextThemesProvider><Component {...pageProps} /></NextThemesProvider>
 }
