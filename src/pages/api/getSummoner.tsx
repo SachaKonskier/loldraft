1d1b0ec75b5e4a44b550de03e48bcb560ede0d3b
SachaK <sacha.konskier@gmail.com>
2024-04-13 18:23:46 +0200

    fix back

--- src/pages/api/getSummoner.tsx
+++ src/pages/api/getSummoner.tsx
@@ -2,14 +2,28 @@ import { NextApiRequest, NextApiResponse } from "next";
 const riotUrl =
   "https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id";
 const apiKey = process.env.RIOT_API_KEY;
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const {query: {summonerName, summonerTag}} = req;
-    if (req.method === 'GET') {
-       const result = await fetch(`${riotUrl}/${summonerName}/${summonerTag}?api_key=${apiKey}`, {method: 'GET', redirect:"follow", }).then((response) => response.json());
-       console.log(result)
-       res.send(result);
-    } 
-    else {
-        res.status(405).send({message: 'Method Not Allowed'});
-    }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const {
+    query: { summonerName, summonerTag },
+  } = req;
+  if (req.method === "GET") {
+    const result = await fetch(
+      `${riotUrl}/${summonerName}/${summonerTag}`,
+      {
+        method: "GET",
+        redirect: "follow",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `X-Riot-Token ${apiKey}`,
+        },
+      }
+    ).then((response) => response.json());
+    console.log(result);
+    res.send(result);
+  } else {
+    res.status(405).send({ message: "Method Not Allowed" });
+  }
 }
