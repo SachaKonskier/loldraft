958fbed277924628291af51bdd9e3b127896612d
SachaK <sacha.konskier@gmail.com>
2024-06-13 01:08:38 +0200

    revert router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,10 @@
 import { Inter } from "next/font/google";
-import { useRouter } from "next/router";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const router = useRouter();
-  function redirect() {
-    router.push("/homepage");
-  }
-  redirect();
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
