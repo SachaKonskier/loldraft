2ffe80977a7393a9b9a9014a6aaef1fc4a3bfafe
SachaK <sacha.konskier@gmail.com>
2025-07-01 19:38:51 +0200

    fix ddragon

--- src/pages/api/riot/matches/index.tsx
+++ src/pages/api/riot/matches/index.tsx
@@ -3,7 +3,7 @@ import { IRefinedChampionOutput } from "@/types/matches/matches";
 import { NextApiRequest, NextApiResponse } from "next";
 const riotUrl = "https://europe.api.riotgames.com/lol/match/v5/matches";
 const apiKey = process.env.RIOT_API_KEY;
-const DDRAGON_VERSION = "14.11.1";
+const DDRAGON_VERSION = "15.13.11";
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -190,7 +190,6 @@ function getKda(kills: number, deaths: number, assists: number) {
   }
   return ((kills + assists) / deaths).toFixed(2);
 }
-// TODO Why is the KDA NAN Sometimes ?
 function mergeData(data: IRefinedChampionOutput[]) {
   const result = data.reduce((acc: any, curr: any) => {
     const champion = acc[curr.championName];
